5ded315f348268bb08f86bf7ac7f01cae3dd7100
zhangtheorem <zhangtheorem@github.com>
2015-04-08 23:11:10 -0700

    added default openGL teapot, testing out rotation, etc

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <fstream>
+#include <cstdio>
+#include <cstdlib>
 
 #include <GL/gl.h>
 #include <GL/glu.h>
@@ -18,48 +20,53 @@ int windowHeight = 700;
 unsigned int mode = GLUT_SINGLE | GLUT_RGB;
 float parameter;
 
+double xAngle = 0;
+double yAngle = 0;
+double zAngle = 0;
+
 GLfloat ctrlpoints[5][3] = {
         { -4.0, -4.0, 0.0}, { -2.0, 4.0, 0.0}, 
         {2.0, -4.0, 0.0}, {4.0, 4.0, 0.0}, {-4.5, 3.0, 0.0}};
 
-void init() {
-    glClearColor(0.0, 0.0, 0.0, 0.0);
-    glShadeModel(GL_FLAT);
-    glMap1f(GL_MAP1_VERTEX_3, 0.0, 1.0, 3, 5, &ctrlpoints[0][0]);
-    glEnable(GL_MAP1_VERTEX_3);
+void init(){
+    glEnable(GL_LIGHTING);      // enable lighting
+    glEnable(GL_LIGHT0);        // enable
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+    float lpos[] = { 1000, 1000, 1000, 1 };
+    glLightfv(GL_LIGHT0, GL_POSITION, lpos);
+
+    glShadeModel(GL_FLAT);       // flat shading
+    //glShadeModel(GL_SMOOTH);        // smooth shading
 }
 
 void display() {
-    int i;
-
+    glClearColor( 0.0, 0.0, 0.0, 0.0 );
     glClear(GL_COLOR_BUFFER_BIT);
-    glColor3f(1.0, 1.0, 1.0);
-    glBegin(GL_LINE_STRIP);
-        for (i = 0; i <= 30; i++) 
-            glEvalCoord1f((GLfloat) i/30.0);
-    glEnd();
-    /* The following code displays the control points as dots. */
-    glPointSize(5.0);
-    glColor3f(1.0, 1.0, 0.0);
-    glBegin(GL_POINTS);
-        for (i = 0; i < 5; i++) 
-            glVertex3fv(&ctrlpoints[i][0]);
-    glEnd();
-    glFlush();
+    glLoadIdentity();
+    glTranslatef(0.0,0.0,-4.5);
+    glPushMatrix();
+    glRotated(yAngle, 0, 1, 0);   //rotate by rotAngle about y-axis
+    glRotated(zAngle, 0, 0, 1);
+    glScalef(1.0,1.0,1.0);
+    glEnable(GL_COLOR_MATERIAL);    // specify object color
+    glColor3f(1.0, 0.1, 0.1);       // redish
+
+    glutSolidTeapot(1);         // draw the teapot
+
+    glPopMatrix();          // restore the modelview matrix
+    glFlush();              // force OpenGL to render now
+
+    glutSwapBuffers();          // make the image visible
 }
 
 void reshape(int w, int h) {
     glViewport(0, 0, (GLsizei) w, (GLsizei) h);
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
-    if (w <= h)
-        glOrtho(-5.0, 5.0, -5.0*(GLfloat)h/(GLfloat)w, 
-                5.0*(GLfloat)h/(GLfloat)w, -5.0, 5.0);
-    else
-        glOrtho(-5.0*(GLfloat)w/(GLfloat)h, 
-                5.0*(GLfloat)w/(GLfloat)h, -5.0, 5.0, -5.0, 5.0);
+    gluPerspective(40.0,(float)w/(float)h, 0.5f, 20.0f);
     glMatrixMode(GL_MODELVIEW);
-    glLoadIdentity();
+
 }
 
 void keyboard(unsigned char key, int x, int y) {
@@ -76,10 +83,11 @@ void keyboard(unsigned char key, int x, int y) {
         case '-':
             std::cout << "-" << std::endl;
             break;
-        case 27:
+        case 'q':
             exit(0);
             break;
     }
+    glutPostRedisplay();
 }
 
 void special(int key, int x, int y) {
@@ -89,6 +97,7 @@ void special(int key, int x, int y) {
                 std::cout << "shift left" << std::endl;
             } else {
                 std::cout << "left" << std::endl;
+                yAngle -= 5;
             }
             break;
         case GLUT_KEY_RIGHT:
@@ -96,6 +105,7 @@ void special(int key, int x, int y) {
                 std::cout << "shift right" << std::endl;
             } else {
                 std::cout << "right" << std::endl;
+                yAngle += 5;
             }
             break;
         case GLUT_KEY_UP:
@@ -103,6 +113,7 @@ void special(int key, int x, int y) {
                 std::cout << "shift up" << std::endl;
             } else {
                 std::cout << "up" << std::endl;
+                zAngle += 5;
             }
             break;
         case GLUT_KEY_DOWN:
@@ -110,9 +121,11 @@ void special(int key, int x, int y) {
                 std::cout << "shift down" << std::endl;
             } else {
                 std::cout << "down" << std::endl;
+                zAngle -= 5;
             }
             break;
     }
+    glutPostRedisplay();
 }
 
 //****************************************************
@@ -161,13 +174,25 @@ void parse_input(char* input) {
 int main(int argc, char *argv[]) {
 
     if (argc < 3) {
-        std::cerr << "Not enought input parameters" << std::endl;
+        //std::cerr << "Not enought input parameters" << std::endl;
         // exit(EXIT_FAILURE);
     }
     // parse_input(argv[1]);
     // parameter = strtof(argv[2], NULL);
 
     glutInit(&argc, argv);
+    glutInitDisplayMode(mode);     // set RGB color mode
+
+    glutCreateWindow("test");   // create the window
+    glutInitWindowSize(windowWidth, windowHeight);
+    init();
+    glutDisplayFunc(display);       // call display() to redraw window
+    glutKeyboardFunc(keyboard);     // call keyboard() when key is hit
+    glutReshapeFunc(reshape);
+    glutSpecialFunc(special);
+    glutMainLoop();         
+    return 0; 
+    /**
     glutInitDisplayMode(mode);
     glutInitWindowSize(windowWidth, windowHeight);
     glutInitWindowPosition(100, 100);
@@ -176,8 +201,8 @@ int main(int argc, char *argv[]) {
     glutDisplayFunc(display);
     glutReshapeFunc(reshape);
     glutKeyboardFunc(keyboard);
-    glutSpecialFunc(special);
+    
     glutMainLoop();
-    return 0;
+    **/
 
 }

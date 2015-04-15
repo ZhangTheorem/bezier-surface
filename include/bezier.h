a7b6268efb3b63896329e7f5016d918931180e81
Elbert Lin <elbertlin168@berkeley.edu>
2015-04-14 21:07:40 -0700

    Removing extraneous code

--- include/bezier.h
+++ include/bezier.h
@@ -22,11 +22,8 @@
 #include <math.h>
 
 class Bezier{
-public:
-	Vector curveinterp(std::vector<Vector> *curve, float u);
-	std::vector<Vector> curveinterpwithdu(std::vector<Vector> *curve, float u);
-	std::vector<Vector> patchinterp(Vector **patch, float u, float v);
-	static void uniform_subdivide(float*** patch, float step, std::vector<float***>* surfaces);
-	void adaptive_subdivide(Vector **patch, float error);
-	static float* bernstein_basis(float** controlPoints, float parameter);
+	public:
+		static void uniform_subdivide(float*** patch, float step, std::vector<float***>* surfaces);
+		void adaptive_subdivide(Vector **patch, float error);
+		static float* bernstein_basis(float** controlPoints, float parameter);
 };
